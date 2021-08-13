3ed7ccc34cb7e47f406f0d60509b6cf12b7ab375
guseppe <52216600+guseppe@users.noreply.github.com>
2021-08-13 09:30:21 -0400

    Datos Curiosos
    Maquetación de datos curiosos

---
+++ Assets/Scripts/maqueta_test.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class maqueta_test : MonoBehaviour
+{
+    public GameObject mensaje_datos_curiosos;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    
+}
